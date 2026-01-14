984a6e9b7e57ce773470100483e04f41fd686eec
Sijan666 <666majharulislam@gmail.com>
2026-01-15 00:49:20 +0600

    add facilities , review , member , contact

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,6 +1,10 @@
 import Banner from "../layouts/Banner";
+import Contact from "../layouts/Contact";
 import Deals from "../layouts/Deals";
+import Faciliti from "../layouts/Faciliti";
 import Featuredproduct from "../layouts/Featuredproduct"
+import Member from "../layouts/Member";
+import Review from "../layouts/Review";
 
 
 const Home = () => {
@@ -9,6 +13,10 @@ const Home = () => {
         <Banner/>
         <Featuredproduct/>
         <Deals/>
+        <Faciliti />
+        <Review />
+        <Member />
+        <Contact />
         </>
     )
 }
