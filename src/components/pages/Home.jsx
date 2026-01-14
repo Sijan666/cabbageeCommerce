e09d13e361d221d1d9d87fa4a08ab4281793dd0d
Nabil <nabil99cc@gmail.com>
2026-01-15 00:43:41 +0600

    ok

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -2,8 +2,12 @@ import Banner from "../layouts/Banner";
 import About from "./About";
 import Service from "./Service";
 import Blog from "./Blog";
-import ContactUs from "./Contact";
+// import ContactUs from "./Contact";
 import Deals from "../layouts/Deals";
+import Faciliti from "../layouts/Faciliti";
+import Review from "../layouts/Review";
+import Member from "../layouts/Member";
+import Contact from "../layouts/Contact";
 
 const Home = () => {
   return (
@@ -12,8 +16,11 @@ const Home = () => {
       <About />
       <Service />
       <Blog />
-      <ContactUs />
       <Deals />
+      <Faciliti />
+      <Review />
+      <Member />
+      <Contact />
     </>
   );
 };
