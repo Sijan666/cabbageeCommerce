2f875836ebd279c3ce99b03a17810d92391caecb
Sijan666 <666majharulislam@gmail.com>
2026-01-16 22:29:08 +0600

    fixed banner problem

--- src/components/layouts/Banner.jsx
+++ src/components/layouts/Banner.jsx
@@ -10,19 +10,62 @@ import "slick-carousel/slick/slick-theme.css";
 import { GoDotFill } from "react-icons/go";
 
 const Banner = () => {
-  const settings = {
+  var settings = {
     dots: true,
     infinite: true,
-    speed: 1000,
+    speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 2000,
+    dotsClass: "slick-dots slider",
   };
 
   return (
-    <div className="slider-container ">
+    <div className="slider-container w-full overflow-hidden">
       <Slider {...settings}>
+        <div className="bg-[url(/src/assets/banner.png)] py-52 bg-no-repeat bg-cover bg-center">
+          <Container>
+            <Flex className={"gap-x-1.5"}>
+              <Images imgSrc={Harb} className={"pb-1"} />
+              <p className="text-[#232323] text-[18px] font-nuni">
+                100% genuine Products
+              </p>
+            </Flex>
+            <h1 className="w-[535px] text-[#232323] font-bold font-int text-[70px] leading-[75px] pt-2.5 pb-9">
+              Tasty & Healthy organic Food
+            </h1>
+            <Button
+              btnText={
+                <span className="flex items-center gap-x-2">
+                  Explore Products
+                  <FaArrowRightLong />
+                </span>
+              }
+            />
+          </Container>
+        </div>
+        <div className="bg-[url(/src/assets/slider.png)] py-52 bg-no-repeat bg-cover bg-center">
+          <Container>
+            <Flex className={"gap-x-1.5"}>
+              <Images imgSrc={Harb} className={"pb-1"} />
+              <p className="text-[#232323] text-[18px] font-nuni">
+                100% genuine Products
+              </p>
+            </Flex>
+            <h1 className="w-[535px] text-[#232323] font-bold font-int text-[70px] leading-[75px] pt-2.5 pb-9">
+              Tasty & Healthy organic Food
+            </h1>
+            <Button
+              btnText={
+                <span className="flex items-center gap-x-2">
+                  Explore Products
+                  <FaArrowRightLong />
+                </span>
+              }
+            />
+          </Container>
+        </div>
         <div className="bg-[url(/src/assets/banner.png)] py-52 bg-no-repeat bg-cover bg-center">
           <Container>
             <Flex className={"gap-x-1.5"}>
