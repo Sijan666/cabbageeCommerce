4136b11675d9fa81333d8b436a3814f722d106cf
Nabil <nabil99cc@gmail.com>
2026-01-20 00:41:05 +0600

    blog start

---
+++ src/components/layouts/Blog.jsx
@@ -0,0 +1,22 @@
+import Container from "../Container";
+
+const Blog = () => {
+  return (
+    <div className="pt-26 pb-48">
+      <Container>
+        <div className="text-center pb-14">
+          <h3 className="text-5xl font-bold text-[#232323] font-int">
+            Leatest Blog
+          </h3>
+          <p className="text-base text-[#546375] font-nuni mt-5">
+            A highly efficient slip-ring scanner for today's diagnostic
+            requirements.
+          </p>
+        </div>
+        <div className=""></div>
+      </Container>
+    </div>
+  );
+};
+
+export default Blog;
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,13 +1,13 @@
 import Banner from "../layouts/Banner";
 import About from "./About";
 import Service from "./Service";
-import Blog from "./Blog";
 // import ContactUs from "./Contact";
 import Deals from "../layouts/Deals";
 import Faciliti from "../layouts/Faciliti";
 import Review from "../layouts/Review";
 import Member from "../layouts/Member";
 import Contact from "../layouts/Contact";
+import Blog from "../layouts/Blog";
 
 const Home = () => {
   return (
