0f58a1425afe9802c432341eaf18d0f568834990
Sijan666 <666majharulislam@gmail.com>
2026-01-23 18:42:49 +0600

    change position of complogo

--- src/components/layouts/CompLogo.jsx
+++ src/components/layouts/CompLogo.jsx
@@ -8,54 +8,56 @@ import For from "/src/assets/vegFor.png";
 
 const CompLogo = () => {
   return (
-    <Container className="bg-white px-[110px] py-12 shadow-blog">
-      <Flex className={"justify-between"}>
-        <Flex className={"gap-x-3.5"}>
-          <Images imgSrc={One} />
-          <div className="">
-            <h5 className="text-[#223645] text-[18px] font-int font-bold pb-2">
-              Curated Products
-            </h5>
-            <p className="text-[#666E77] text-[12px] font-nuni">
-              From handpicked sellers
-            </p>
-          </div>
+    <div className="relative">
+      <Container className="bg-white px-[110px] py-12 shadow-blog w-full absolute -bottom-18 left-1/2 -translate-x-1/2">
+        <Flex className={"justify-between"}>
+          <Flex className={"gap-x-3.5"}>
+            <Images imgSrc={One} />
+            <div className="">
+              <h5 className="text-[#223645] text-[18px] font-int font-bold pb-2">
+                Curated Products
+              </h5>
+              <p className="text-[#666E77] text-[12px] font-nuni">
+                From handpicked sellers
+              </p>
+            </div>
+          </Flex>
+          <Flex className={"gap-x-3.5"}>
+            <Images imgSrc={Two} />
+            <div className="">
+              <h5 className="text-[#223645] text-[18px] font-int font-bold pb-2">
+                Handmade
+              </h5>
+              <p className="text-[#666E77] text-[12px] font-nuni">
+                From handpicked sellers
+              </p>
+            </div>
+          </Flex>
+          <Flex className={"gap-x-3.5"}>
+            <Images imgSrc={Thr} />
+            <div className="">
+              <h5 className="text-[#223645] text-[18px] font-int font-bold pb-2">
+                100% Natural
+              </h5>
+              <p className="text-[#666E77] text-[12px] font-nuni">
+                From handpicked sellers
+              </p>
+            </div>
+          </Flex>
+          <Flex className={"gap-x-3.5"}>
+            <Images imgSrc={For} />
+            <div className="">
+              <h5 className="text-[#223645] text-[18px] font-int font-bold pb-2">
+                Shipping
+              </h5>
+              <p className="text-[#666E77] text-[12px] font-nuni">
+                From handpicked sellers
+              </p>
+            </div>
+          </Flex>
         </Flex>
-        <Flex className={"gap-x-3.5"}>
-          <Images imgSrc={Two} />
-          <div className="">
-            <h5 className="text-[#223645] text-[18px] font-int font-bold pb-2">
-              Handmade
-            </h5>
-            <p className="text-[#666E77] text-[12px] font-nuni">
-              From handpicked sellers
-            </p>
-          </div>
-        </Flex>
-        <Flex className={"gap-x-3.5"}>
-          <Images imgSrc={Thr} />
-          <div className="">
-            <h5 className="text-[#223645] text-[18px] font-int font-bold pb-2">
-              100% Natural
-            </h5>
-            <p className="text-[#666E77] text-[12px] font-nuni">
-              From handpicked sellers
-            </p>
-          </div>
-        </Flex>
-        <Flex className={"gap-x-3.5"}>
-          <Images imgSrc={For} />
-          <div className="">
-            <h5 className="text-[#223645] text-[18px] font-int font-bold pb-2">
-              Shipping
-            </h5>
-            <p className="text-[#666E77] text-[12px] font-nuni">
-              From handpicked sellers
-            </p>
-          </div>
-        </Flex>
-      </Flex>
-    </Container>
+      </Container>
+    </div>
   );
 };
 
