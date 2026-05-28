6749e207ae95e3e4aebb38d3ddf6662b14657703
Sijan666 <666majharulislam@gmail.com>
2026-05-28 21:11:53 +0600

    fix responsive issue in topCategories section

--- src/components/layouts/Topcategories.jsx
+++ src/components/layouts/Topcategories.jsx
@@ -1,17 +1,16 @@
 import React from 'react'
 import Container from '../Container'
-import Flex from '../Flex'
 
 const Topcategories = () => {
     return (
         <>
-        <div className="my-30 bg-[#525355] pt-25 pb-[290px] ">
-            <Container>
+        <div className="my-16 md:my-30 bg-[#525355] pt-16 md:pt-25 pb-[150px] md:pb-[290px]">
+            <Container className="px-4 lg:px-0">
                 <div className="text-center mb-1">
-                    <h3 className="text-5xl font-bold text-[#FFFFFF] font-int">
+                    <h3 className="text-3xl md:text-5xl font-bold text-[#FFFFFF] font-int">
                         Top Categories
                     </h3>
-                    <p className="text-base text-[#FFFFFF] font-nuni mt-5">
+                    <p className="text-sm md:text-base text-[#FFFFFF] font-nuni mt-3 md:mt-5 max-w-2xl mx-auto">
                         A highly efficient slip-ring scanner for today's diagnostic
                         requirements.
                     </p>
@@ -22,4 +21,4 @@ const Topcategories = () => {
     )
 }
 
-export default Topcategories
+export default Topcategories;
