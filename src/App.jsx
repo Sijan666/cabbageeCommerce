110ce6fc16634c47d72e29fb7976274089ead3de
Sijan666 <666majharulislam@gmail.com>
2026-06-06 02:34:05 +0600

    add lenis for premium scrolling behavior

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,31 @@ import { Routes , Route } from 'react-router-dom'
 import RootLayouts from './components/layouts/RootLayouts'
 import Error from './components/pages/Error'
 
-
+import { ReactLenis } from 'lenis/react'
+import 'lenis/dist/lenis.css'
 
 function App () {
+
+  const lenisOptions = {
+    lerp: 0.08,
+    smoothWheel: true,
+    syncTouch: true,
+    wheelMultiplier: 1,
+    touchMultiplier: 2, 
+  }
+
   return (
     <>
-      <Routes>
-        <Route path="/" element={<RootLayouts />}>
-          <Route index element={<Home/>} />
-        </Route>
+      <ReactLenis root options={lenisOptions}>
+        <Routes>
+          <Route path="/" element={<RootLayouts />}>
+            <Route index element={<Home/>} />
+          </Route>
           <Route path="*" element={<Error/>} />
-      </Routes>
+        </Routes>
+      </ReactLenis>
     </>
   )
 }
 
 export default App
-
-
