431ed4b60da9acd85b1322c6222511ee8c4ba39d
Sijan666 <666majharulislam@gmail.com>
2026-06-06 02:46:34 +0600

    add react helmet for seo

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,9 +1,10 @@
+import { Helmet } from "react-helmet-async";
 import Ads from "../layouts/Ads";
 import Banner from "../layouts/Banner";
 import Contact from "../layouts/Contact";
 import Deals from "../layouts/Deals";
 import Faciliti from "../layouts/Faciliti";
-import Featuredproduct from "../layouts/Featuredproduct"
+import Featuredproduct from "../layouts/Featuredproduct";
 import Member from "../layouts/Member";
 import Review from "../layouts/Review";
 import Ourproduct from "../layouts/Ourproducts";
@@ -14,25 +15,29 @@ import Counter from "../layouts/Counter";
 import Blog from "../layouts/Blog";
 import CompLogo from "../layouts/CompLogo";
 
-
 const Home = () => {
     return (
         <>
-        <Banner/>
-        <Ads/>
-        <Featuredproduct/>
-        <Deals/>
-        <Ourproduct/>
-        <Topcategories/>
-        <Topslider/>
-        <Faciliti />
-        <Review />
-        <Member />
-        <Contact />
-        <Faq/>
-        <Counter/>
-        <Blog/>
-        <CompLogo/>
+            <Helmet>
+                <title>Home | Cabbage eCommerce</title>
+                <meta name="description" content="Welcome to our website. Discover our best deals, featured products, and top categories." />
+                <meta name="keywords" content="ecommerce, shop, best products, deals" />
+            </Helmet>
+            <Banner/>
+            <Ads/>
+            <Featuredproduct/>
+            <Deals/>
+            <Ourproduct/>
+            <Topcategories/>
+            <Topslider/>
+            <Faciliti />
+            <Review />
+            <Member />
+            <Contact />
+            <Faq/>
+            <Counter/>
+            <Blog/>
+            <CompLogo/>
         </>
     )
 }
