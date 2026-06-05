3ea37bcf1e1c5c5d59fd6773ca58e34e838e09b8
Sijan666 <666majharulislam@gmail.com>
2026-06-06 02:23:27 +0600

    remove unwanted components and codes

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import './App.css'
 import Home from './components/pages/Home'
 import { Routes , Route } from 'react-router-dom'
 import RootLayouts from './components/layouts/RootLayouts'
-// import About from './components/pages/About'
-// import Error from './components/pages/Error'
+import Error from './components/pages/Error'
+
 
 
 function App () {
@@ -12,9 +12,8 @@ function App () {
       <Routes>
         <Route path="/" element={<RootLayouts />}>
           <Route index element={<Home/>} />
-          {/* <Route path="/about" element={<About/>} /> */}
         </Route>
-          {/* <Route path="*" element={<Error/>} /> */}
+          <Route path="*" element={<Error/>} />
       </Routes>
     </>
   )
