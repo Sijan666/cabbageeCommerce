ca8a6f0829471f85e53cdf5b14074b4fe2b00726
Sijan666 <666majharulislam@gmail.com>
2026-07-29 23:58:48 +0600

    refactor: optimize CompLogo component with map and responsive grid

--- src/components/layouts/CompLogo.jsx
+++ src/components/layouts/CompLogo.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import Container from "../Container";
 import Flex from "../Flex";
 import Images from "../Images";
@@ -7,54 +8,54 @@ import Thr from "/src/assets/vegThr.png";
 import For from "/src/assets/vegFor.png";
 
 const CompLogo = () => {
+  const features = [
+    {
+      id: 1,
+      img: One,
+      title: "Curated Products",
+      desc: "From handpicked sellers",
+    },
+    {
+      id: 2,
+      img: Two,
+      title: "Handmade",
+      desc: "Crafted with love & care",
+    },
+    {
+      id: 3,
+      img: Thr,
+      title: "100% Natural",
+      desc: "No synthetic additives",
+    },
+    {
+      id: 4,
+      img: For,
+      title: "Fast Shipping",
+      desc: "Safe & secure delivery",
+    },
+  ];
+
   return (
-    <div className="lg:relative">
-      <Container className="bg-white lg:px-[110px] py-12 shadow-blog w-full lg:absolute lg:-bottom-18 lg:left-1/2 lg:-translate-x-1/2">
-        <div className={"flex flex-col lg:flex-row items-center gap-y-10 lg:justify-between px-3 lg:px-0"}>
-          <Flex className={"gap-x-3.5"}>
-            <Images imgSrc={One} />
-            <div className="">
-              <h5 className="text-[#223645] text-[18px] font-int font-bold pb-2">
-                Curated Products
-              </h5>
-              <p className="text-[#666E77] text-[12px] font-nuni">
-                From handpicked sellers
-              </p>
-            </div>
-          </Flex>
-          <Flex className={"gap-x-3.5"}>
-            <Images imgSrc={Two} />
-            <div className="">
-              <h5 className="text-[#223645] text-[18px] font-int font-bold pb-2">
-                Handmade
-              </h5>
-              <p className="text-[#666E77] text-[12px] font-nuni">
-                From handpicked sellers
-              </p>
-            </div>
-          </Flex>
-          <Flex className={"gap-x-3.5"}>
-            <Images imgSrc={Thr} />
-            <div className="">
-              <h5 className="text-[#223645] text-[18px] font-int font-bold pb-2">
-                100% Natural
-              </h5>
-              <p className="text-[#666E77] text-[12px] font-nuni">
-                From handpicked sellers
-              </p>
-            </div>
-          </Flex>
-          <Flex className={"gap-x-3.5"}>
-            <Images imgSrc={For} />
-            <div className="">
-              <h5 className="text-[#223645] text-[18px] font-int font-bold pb-2">
-                Shipping
-              </h5>
-              <p className="text-[#666E77] text-[12px] font-nuni">
-                From handpicked sellers
-              </p>
-            </div>
-          </Flex>
+    <div className="lg:relative z-20">
+      {/* Banner */}
+      <Container className="bg-white lg:px-12 xl:px-[110px] py-10 lg:py-12 shadow-[0_10px_40px_rgba(0,0,0,0.05)] border border-gray-100 rounded-2xl w-full lg:absolute lg:-bottom-18 lg:left-1/2 lg:-translate-x-1/2">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-10 lg:gap-6 px-4 lg:px-0">
+          {features.map((feature) => (
+            <Flex key={feature.id} className="items-center gap-x-4 lg:justify-center group cursor-pointer">
+              <div className="w-14 h-14 shrink-0 bg-[#F9FBF5] rounded-full flex justify-center items-center group-hover:bg-[#80B500]/10 transition-colors duration-300">
+                <Images imgSrc={feature.img} className="w-8 h-8 object-contain" />
+              </div>
+              {/* Text */}
+              <div>
+                <h5 className="text-[#232323] text-[18px] font-int font-bold pb-1 group-hover:text-[#80B500] transition-colors duration-300">
+                  {feature.title}
+                </h5>
+                <p className="text-[#546375] text-[13px] font-nuni">
+                  {feature.desc}
+                </p>
+              </div>
+            </Flex>
+          ))}
         </div>
       </Container>
     </div>
