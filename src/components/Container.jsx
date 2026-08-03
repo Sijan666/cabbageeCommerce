83eb744a927d89ca9605d6065cbff382b9e2f6a5
Sijan666 <666majharulislam@gmail.com>
2026-08-03 23:53:40 +0600

    fix : width size for container

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,7 +1,7 @@
 
 const Container = ({children , className}) => {
     return (
-        <div className={`max-w-[1170px] mx-auto ${className}`}>{children}</div>
+        <div className={`max-w-292.5 mx-auto ${className}`}>{children}</div>
     )
 }
 
