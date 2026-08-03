b81cbd851e08d9baf66787d90e99d31befa1911d
Sijan666 <666majharulislam@gmail.com>
2026-08-03 23:54:38 +0600

    fix : position for arrows

--- src/components/NextArrow.jsx
+++ src/components/NextArrow.jsx
@@ -7,7 +7,7 @@ const NextArrow = (props) => {
     const { onClick } = props;
 
     return (
-        <div className="absolute top-[50%] -right-55 -translate-y-[30%] shadow-lg z-10 cursor-pointer p-4 rounded-full hover:bg-[#80B500] bg-white duration-300 text-[#80B500] hover:text-white" onClick={onClick}>
+        <div className="absolute top-[50%] -right-55 translate-y-[-30%] shadow-lg z-10 cursor-pointer p-4 rounded-full hover:bg-[#80B500] bg-white duration-300 text-[#80B500] hover:text-white" onClick={onClick}>
             <IoIosArrowForward  className="text-4xl" />
         </div>
     );
--- src/components/PrevArrow.jsx
+++ src/components/PrevArrow.jsx
@@ -5,7 +5,7 @@ const PrevArrow = (props) => {
     const { onClick } = props;
 
     return (
-        <div className="absolute top-[50%] -left-55 -translate-y-[30%] shadow-lg z-10 cursor-pointer p-4 rounded-full hover:bg-[#80B500] bg-white duration-300 text-[#80B500] hover:text-white" onClick={onClick}>
+        <div className="absolute top-[50%] -left-55 translate-y-[-30%] shadow-lg z-10 cursor-pointer p-4 rounded-full hover:bg-[#80B500] bg-white duration-300 text-[#80B500] hover:text-white" onClick={onClick}>
             <IoIosArrowBack  className="text-4xl "/>
         </div>
     );
