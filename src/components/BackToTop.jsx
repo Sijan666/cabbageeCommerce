f4ec92e09c4b9a16581ef5a9e1137e3e6f30944a
Majharul Islam <666majharulislam@gmail.com>
2026-08-08 22:16:35 +0600

    update : backtotop ui

--- src/components/BackToTop.jsx
+++ src/components/BackToTop.jsx
@@ -78,7 +78,7 @@ const BackToTop = () => {
                 }}
             />
             </svg>
-            {/* arrow */}
+            arrow
             <div className="relative flex items-center justify-center w-11 h-11 bg-[#F4F9EB] text-[#80B500] rounded-full group-hover:bg-[#80B500] group-hover:text-white transition-colors duration-300">
             <svg 
                 className="w-5 h-5 transform group-hover:-translate-y-1 transition-transform duration-300" 
