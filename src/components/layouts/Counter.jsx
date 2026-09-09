4a7d048a92ab395aa87868d5ad09b9744ac5697b
Sijan666 <666majharulislam@gmail.com>
2026-09-09 23:44:38 +0600

    style : implement responsive css grid layout for features

--- src/components/layouts/Counter.jsx
+++ src/components/layouts/Counter.jsx
@@ -13,31 +13,47 @@ import SlotCounter from 'react-slot-counter';
 const Counter = () => {
     return (
         <>
-        <div className="lg:bg-[url('/src/assets/counter.png')] bg-no-repeat bg-center bg-cover py-18">
-            <Container className={'px-3 lg:px-0'}>
-                <div className={'flex flex-col lg:flex-row gap-y-10 items-center lg:justify-between'}>
-                    <div className="mx-auto text-center">
+        <div className="lg:bg-[url('/src/assets/counter.png')] bg-no-repeat bg-center bg-cover py-12 sm:py-16 lg:py-18">
+            <Container className={'px-4 sm:px-6 lg:px-0'}>
+                {/* main wrapper */}
+                <div className="grid grid-cols-2 sm:grid-cols-2 lg:flex lg:flex-row gap-y-10 lg:gap-y-0 items-center justify-between w-full">
+                    {/* counter 1 */}
+                    <div className="mx-auto text-center w-full lg:w-auto">
                         <Images imgSrc={c1} className={'mx-auto'}/>
-                        <h4 className='text-[42px] font-bold font-int text-[#223645] pt-5 pb-2.5'><SlotCounter value={560} animateOnVisible={{ triggerOnce: false, rootMargin: '0px 0px -100px 0px' }} /></h4>
-                        <p className='text-[#666E77] text-[19px] font-nuni'>Good Reviews</p>
+                        <h4 className='text-4xl lg:text-[42px] font-bold font-int text-[#223645] pt-4 lg:pt-5 pb-1.5 lg:pb-2.5'>
+                            <SlotCounter value={560} animateOnVisible={{ triggerOnce: false, rootMargin: '0px 0px -100px 0px' }} />
+                        </h4>
+                        <p className='text-[#666E77] text-base lg:text-[19px] font-nuni'>Good Reviews</p>
                     </div>
-                    <Images imgSrc={path} className={'hidden md:block'}/>
-                    <div className="mx-auto text-center">
+                    {/* path separator */}
+                    <Images imgSrc={path} className={'hidden lg:block shrink-0'}/>
+                    {/* counter 2 */}
+                    <div className="mx-auto text-center w-full lg:w-auto">
                         <Images imgSrc={c2} className={'mx-auto'}/>
-                        <h4 className='text-[42px] font-bold font-int text-[#223645] pt-5 pb-2.5'><SlotCounter value={197} animateOnVisible={{ triggerOnce: false, rootMargin: '0px 0px -100px 0px' }}/></h4>
-                        <p className='text-[#666E77] text-[19px] font-nuni'>Organic Branch</p>
+                        <h4 className='text-4xl lg:text-[42px] font-bold font-int text-[#223645] pt-4 lg:pt-5 pb-1.5 lg:pb-2.5'>
+                            <SlotCounter value={197} animateOnVisible={{ triggerOnce: false, rootMargin: '0px 0px -100px 0px' }}/>
+                        </h4>
+                        <p className='text-[#666E77] text-base lg:text-[19px] font-nuni'>Organic Branch</p>
                     </div>
-                    <Images imgSrc={path} className={'hidden md:block'}/>
-                    <div className="mx-auto text-center">
+                    {/* path separator */}
+                    <Images imgSrc={path} className={'hidden lg:block shrink-0'}/>
+                    {/* counter 3 */}
+                    <div className="mx-auto text-center w-full lg:w-auto">
                         <Images imgSrc={c3} className={'mx-auto'}/>
-                        <h4 className='text-[42px] font-bold font-int text-[#223645] pt-5 pb-2.5'><SlotCounter value={268} animateOnVisible={{ triggerOnce: false, rootMargin: '0px 0px -100px 0px' }}/></h4>
-                        <p className='text-[#666E77] text-[19px] font-nuni'>Cultivator Land</p>
+                        <h4 className='text-4xl lg:text-[42px] font-bold font-int text-[#223645] pt-4 lg:pt-5 pb-1.5 lg:pb-2.5'>
+                            <SlotCounter value={268} animateOnVisible={{ triggerOnce: false, rootMargin: '0px 0px -100px 0px' }}/>
+                        </h4>
+                        <p className='text-[#666E77] text-base lg:text-[19px] font-nuni'>Cultivator Land</p>
                     </div>
-                    <Images imgSrc={path} className={'hidden md:block'}/>
-                    <div className="mx-auto text-center">
+                    {/* path separator */}
+                    <Images imgSrc={path} className={'hidden lg:block shrink-0'}/>
+                    {/* counter 4 */}
+                    <div className="mx-auto text-center w-full lg:w-auto">
                         <Images imgSrc={c4} className={'mx-auto'}/>
-                        <h4 className='text-[42px] font-bold font-int text-[#223645] pt-5 pb-2.5'><SlotCounter value={340} animateOnVisible={{ triggerOnce: false, rootMargin: '0px 0px -100px 0px' }}/></h4>
-                        <p className='text-[#666E77] text-[19px] font-nuni'>Total Consumer</p>
+                        <h4 className='text-4xl lg:text-[42px] font-bold font-int text-[#223645] pt-4 lg:pt-5 pb-1.5 lg:pb-2.5'>
+                            <SlotCounter value={340} animateOnVisible={{ triggerOnce: false, rootMargin: '0px 0px -100px 0px' }}/>
+                        </h4>
+                        <p className='text-[#666E77] text-base lg:text-[19px] font-nuni'>Total Consumer</p>
                     </div>
                 </div>
             </Container>
